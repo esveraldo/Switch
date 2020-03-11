845b40c8671c68a82ffe37f234e232760bb065c7
esveraldo <esveraldo@hotmail.com>
2020-03-11 09:33:59 -0300

    Configurando o acesso ao banco de dados na web API

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,19 @@
+﻿using Switch.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataDeNascimento { get; set; }
+        public string Foto { get; set; }
+        public SexoEnum Sexo { get; set; }
+    }
+}
---
+++ Switch.Domain/Enums/SexoEnum.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Switch.Domain.Enums
+{
+    public enum SexoEnum
+    {
+        NãoDefinido = 0,
+        Masculino = 1,
+        Feminino = 2,
+    }
+}
